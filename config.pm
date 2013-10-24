6e42976362d7e9396fc937aabc5b46b4802a9380
Eotect Nahn <eotect@gmail.com>
2013-10-25 02:49:53 +0800

    Add config file.
    	Add file: config.pm

---
+++ config.pm
@@ -0,0 +1,17 @@
+
+$MyPlace::URLRule::Config = {
+	'maps.domain'=>{
+		'gangtiexia3.com'=>'site.qvod',
+		'bibi138.com'=>'site.qvod.adult',
+		'01aiai.com'=>'site.qvod.adult',
+		'se1199.com'=>'site.qvod.adult',
+		'qvod123.net'=>'site.qvod',
+		'www.qvod123.net'=>'site.qvod',
+		'eeuus.com'=>'site.qvod.adult',
+		'wse123.com'=>'site.qvod.adult',
+		'350hh.com'=>'site.qvod.adult',
+		'av3030.com'=>'site.qvod.adult',
+	},
+};
+
+1;
